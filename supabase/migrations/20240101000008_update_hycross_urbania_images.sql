0adc851051756857d40b77abd588b66d075090c8
Pahul <royalpahul@gmail.com>
2026-06-27 17:12:47 +0530

    update

---
+++ supabase/migrations/20240101000008_update_hycross_urbania_images.sql
@@ -0,0 +1,2 @@
+UPDATE fleet SET image_url = '/hycross.jpg' WHERE name = 'Toyota Innova Hycross';
+UPDATE fleet SET image_url = '/urbania.jpg' WHERE name = 'Force Urbania Luxury';
---
+++ supabase/migrations/20240101000009_update_hycross_urbania_images_png.sql
@@ -0,0 +1,2 @@
+UPDATE fleet SET image_url = '/hycross.png' WHERE name = 'Toyota Innova Hycross';
+UPDATE fleet SET image_url = '/urbania.png' WHERE name = 'Force Urbania Luxury';

20a609ec83df5faec44227a0012bf96c6eaddcc1
Pahul <royalpahul@gmail.com>
2026-07-17 23:16:38 +0530

    update

--- test-fleet.ts
+++
@@ -1,7 +0,0 @@
-import { supabaseAdmin } from './src/lib/supabase-admin';
-
-async function run() {
-  const { data, error } = await supabaseAdmin.from('fleet').select('*');
-  console.log(data);
-}
-run();
--- test-pricing.ts
+++
@@ -1,7 +0,0 @@
-import { supabaseAdmin } from './src/lib/supabase-admin';
-
-async function run() {
-  const { data, error } = await supabaseAdmin.from('pricing_routes').select('*');
-  console.log(data);
-}
-run();
